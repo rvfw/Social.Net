38bc272cbe33f1efc1d2f23dfa084ae54440310e
rvfw <k.daniil2506@gmail.com>
2025-05-17 20:34:43 +0400

    Test1

---
+++ Messenger/Contollers/CommentsController.cs
@@ -0,0 +1,72 @@
+﻿using Messenger.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Messenger.Contollers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CommentsController : Controller
+    {
+        private readonly MessengerDbContext _context;
+        public CommentsController(MessengerDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("{postId}")]
+        public IActionResult GetComments(int postId,int offset=0,int limit=50) 
+        {
+            if (limit < 0 || offset < 0)
+                return BadRequest();
+            if (_context.Posts.FirstOrDefault(x => x.Id == postId)==null)
+                return BadRequest();
+            return Ok(_context.Comments.Where(x=>x.PostId==postId).Skip(offset).Take(Math.Min(50, limit)));
+        }
+        [HttpPost("{postId}")]
+        public IActionResult CreateComment(int postId,[FromBody] CommentRequest request)
+        {
+            var post= _context.Posts.FirstOrDefault(x=>x.Id== postId);
+            if(post == null || _context.Users.FirstOrDefault(x => x.Id == request.UserId) == null)
+                return BadRequest();
+            if (!AuthController.CheckToken(request.Token, request.UserId))
+                return Forbid();
+            var comment = new Comment(request.UserId,postId, request.Text);
+            _context.Comments.Add(comment);
+            _context.SaveChanges();
+            return CreatedAtAction(null, comment.Id,comment);
+        }
+        [HttpPut("{postId}")]
+        public IActionResult UpdateComment(int postId, [FromBody] CommentRequest request)
+        {
+            var comment = _context.Comments.FirstOrDefault(x => x.Id == request.Id);
+            if (comment == null)
+                return BadRequest();
+            if (comment.Id != request.UserId || !AuthController.CheckToken(request.Token, request.UserId))
+                return Forbid();
+            comment.Text = request.Text; 
+            _context.SaveChanges();
+            return Ok(comment);
+        }
+        [HttpDelete("{postId}")]
+        public IActionResult DeleteComment(int postId, [FromBody] CommentRequest request)
+        {
+            var comment = _context.Comments.FirstOrDefault(x => x.Id == request.Id);
+            if (comment == null)
+                return BadRequest();
+            if (comment.Id != request.UserId || !AuthController.CheckToken(request.Token, request.UserId))
+                return Forbid();
+            _context.Comments.Remove(comment);
+            return Ok(comment);
+        }
+    }
+    public class CommentRequest
+    {
+        public int Id { get; set; }
+        public string Token {  get; set; }
+        public int UserId {  get; set; }
+        public string? Text { get; set; }
+    }
+}
---
+++ Messenger/Contollers/PostsController.cs
@@ -0,0 +1,64 @@
+﻿using Messenger.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Messenger.Contollers
+{
+    [ApiController]
+    [Route("api/{controller}")]
+    public class PostsController : Controller
+    {
+        private readonly MessengerDbContext _context;
+        public PostsController(MessengerDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("{postId}")]
+        public IActionResult GetPost(int postId)
+        {
+            var post=_context.Posts.FirstOrDefault(x => x.Id == postId);
+            if (post == null)
+                return BadRequest();
+            return Ok(post);
+        }
+        [HttpGet]
+        public IActionResult GetPosts(int offset=0,int limit=50)
+        {
+            if(limit<0 || offset<0)
+                return BadRequest();
+            return Ok(_context.Posts.AsQueryable().Skip(offset).Take(Math.Min(50,limit)));
+        }
+        [HttpPut("{postId}")]
+        public IActionResult UpdatePost(int postId, [FromBody] PostRequest post)
+        {
+            var oldPost = _context.Posts.FirstOrDefault(x => x.Id == postId);
+            if (oldPost == null)
+                return BadRequest();
+            if (oldPost.UserId != post.UserId || !AuthController.CheckToken(post.Token, post.UserId))
+                return Forbid();
+            oldPost.Text=post.Text;
+            _context.SaveChanges();
+            return Ok(oldPost);
+        }
+        [HttpDelete("{postId}")]
+        public IActionResult DeletePost(int postId, [FromBody]PostRequest post)
+        {
+            var oldPost= _context.Posts.FirstOrDefault(x=>x.Id==postId);
+            if(oldPost == null)
+                return BadRequest();
+            if(oldPost.UserId!= post.UserId || !AuthController.CheckToken(post.Token,post.UserId))
+                return Forbid();
+            _context.Users.First(x => x.Id == post.UserId).Posts.Remove(oldPost);
+            _context.SaveChanges();
+            return Ok(oldPost);
+        }
+    }
+    public class PostRequest
+    {
+        public int Id { get; set; }
+        public string? Text { get; set; }
+        public string Token { get; set; }
+        public int UserId { get; set; }
+    }
+}
